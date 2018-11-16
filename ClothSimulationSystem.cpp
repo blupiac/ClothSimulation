5283a4e9695c451ac15d90070b32a2ad0e2ff37b
blupiac <blupiac@gmail.com>
2018-11-15 22:20:25 -0200

    initial commit

---
+++ ClothSimulationSystem.cpp
@@ -0,0 +1,8 @@
+//-----------------------------------------------------------------------------
+// Author: Bernard Lupiac
+// Created: 15/11/2018
+// Based on: https://www.gamasutra.com/view/feature/131313/advanced_character_physics.php
+// and class notes
+//-----------------------------------------------------------------------------
+
+#include "ClothSimulationSystem.hpp"
